1234f6698330c4ae787d185e3f24d4f8ad5bf161
gfbatista <gilbertoferrari@gmail.com>
2024-09-26 10:55:14 -0300

    chore: initial commit

---
+++ src/main/kotlin/com/br/taximeapi/TaximeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.br.taximeapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaximeApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaximeApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/taximeapi/TaximeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.taximeapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaximeApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
